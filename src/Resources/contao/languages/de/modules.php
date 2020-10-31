30a706e6a046ad7b57e24672aa759ac8a9080a1e
frfr <frfr@spreadshirt.net>
2020-10-31 13:47:02 +0100

    fix trans

--- src/Resources/contao/languages/de/modules.php
+++ src/Resources/contao/languages/de/modules.php
@@ -1,3 +1,4 @@
 <?php
-$GLOBALS['TL_LANG']['FMD']['courseSchedule'] = ['Course Schedule', 'Soll mal dem aktuelen Kursplan ausgeben'];
-$GLOBALS['TL_LANG']['FMD']['courseList'] = ['Course List', 'Soll mal die Liste der aktuelen Kurse ausgeben'];
+
+$GLOBALS['TL_LANG']['FMD']['courseSchedule'] = ['Kursplan', 'Soll mal dem aktuelen Kursplan ausgeben'];
+$GLOBALS['TL_LANG']['FMD']['courseList'] = ['Kursliste', 'Soll mal die Liste der aktuelen Kurse ausgeben'];
