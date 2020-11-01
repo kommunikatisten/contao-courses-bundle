7d08891ae7c34b7e0f7622adb9f1fecfdf4b366b
frfr <frfr@spreadshirt.net>
2020-11-01 10:52:44 +0100

    restart new

--- src/Resources/contao/dca/tl_module.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$GLOBALS['TL_DCA']['tl_module']['palettes']['courseschedule'] = '{title_legend},name,headline,type;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
-$GLOBALS['TL_DCA']['tl_module']['palettes']['courselist'] = '{title_legend},name,headline,type;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
--- src/Resources/contao/languages/de/modules.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$GLOBALS['TL_LANG']['FMD']['courseschedule'] = ['Kursplan', 'Soll mal dem aktuelen Kursplan ausgeben'];
-$GLOBALS['TL_LANG']['FMD']['courselist'] = ['Kursliste', 'Soll mal die Liste der aktuelen Kurse ausgeben'];
